fd7d572852938a190d5200ad0567fb5e19735279
Pedro <ptr.bushi@hotmail.com>
2021-07-01 03:38:38 -0300

    :sparkles: feat: add requests to api

---
+++ constants/api.js
@@ -0,0 +1,2 @@
+const API_KEY = "925eba28";
+export const GET_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`;
---
+++ src/services/requests.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { GET_URL } from "../../constants/api";
+
+export const getService = (url, params, headers) =>
+  axios
+    .get(url, { params, headers })
+    .then(r => r)
+    .catch(e => console.log("FLAG E1", e));
+
+export const getSearch = search => getService(GET_URL + search+"&page=2").then(r => r);
