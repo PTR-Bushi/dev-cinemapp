68e736c71888671e1346e6bfb5418c233e7b7830
Pedro <ptr.bushi@hotmail.com>
2021-07-01 22:44:26 -0300

    :art: style: make dimesions responsive
    should prevent weird wraps

--- constants/dimensions.js
+++ constants/dimensions.js
@@ -3,7 +3,7 @@ import { Dimensions } from "react-native";
 const dims = Dimensions.get("window");
 const WIDTH = dims.width;
 
-export const generalPadding = 10;
+export const generalPadding = WIDTH * 0.03;
 
 export const titleSize = WIDTH * (1 / 20);
 export const textSize = WIDTH * (1 / 25);
@@ -12,9 +12,9 @@ export const boxPadding = generalPadding / 2;
 export const boxBorder = 1;
 export const boxWidth = "100%";
 
-export const componentsSeparation = 50;
+export const componentsSeparation = WIDTH * 0.144;
 
-export const headerLeftSize = 25;
+export const headerLeftSize = WIDTH * 0.072;
 
 export const posterWidthBig = WIDTH * 0.3;
 export const posterHeightBig = posterWidthBig * 1.44;
